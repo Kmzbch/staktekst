95a1bb6d259bdded58b4f4b776e09b725390ae0e
Kei Mizubuchi <kmizubuc@my.centennialcollege.ca>
2019-12-15 18:20:25 -0500

    refactored popup.js

--- src/popup.js
+++ src/popup.js
@@ -1,77 +1,33 @@
 import './popup.css';
 
+const addStackSpace = document.getElementById("addStackSpace");
+const addTextArea = document.getElementById("addTextArea");
+const textarea = document.getElementById("addStackText");
+const footer = document.getElementsByTagName("footer")[0];
+const resetBtn = document.getElementById('resetBtn');
+const list = document.getElementsByClassName('itemlist')[0];
+
 (function () {
-
-
-
-
-  let addStackSpace = document.getElementById("addStackSpace");
-  let addTextArea = document.getElementById("addTextArea");
-  let textarea = document.getElementById("addStackText");
-
-  let footer = document.getElementsByTagName("footer")[0];
-
-  footer.addEventListener("click", () => {
-    window.scrollBy(0, 300);
-    var scrollHeight, totalHeight;
-    scrollHeight = document.body.scrollHeight;
-    totalHeight = window.scrollY + window.innerHeight;
-
-    if (totalHeight >= scrollHeight) {
-      let scrollButton = document.getElementById("scrollButton");
-      console.log(scrollButton);
-      footer.removeChild(scrollButton);
-      let newScrollButton = document.createElement("i");
-      newScrollButton.setAttribute("class", "material-icons");
-      newScrollButton.innerText = "arrow_upward";
-      footer.appendChild(newScrollButton);
-      console.log("at the bottom");
-    }
-  });
-
-
+  // initialize eventListeners
   textarea.addEventListener("mouseout", (e) => {
     textarea.style.display = "none";
     addTextArea.style.display = "block";
-
   });
-  textarea.addEventListener("keyup", (e) => {
 
-    console.log("asdfasd");
+  textarea.addEventListener("keyup", (e) => {
     // If the user has pressed enter
     if (e.keyCode === 13 + e.shiftKey) {
-      let newText = document.getElementById('addStackText').value;
-
+      let newText = textarea.value;
       let newUrl = "";
       updateStack({
         type: 'ADD',
         text: newText,
         url: newUrl
       });
-      let listDOM = document.getElementsByClassName('itemlist')[0];
       var elem = document.createElement("li");
       elem.textContent = newText;
-
-      elem.addEventListener("dblclick", () => {
-        console.log("!!!")
-        var nodes = document.getElementsByTagName('li');
-        var parent = document.getElementsByTagName('ul')[0];
-        // plus 1
-        parent.removeChild(nodes[nodes.length]);
-      })
-
-      if (elem.textContent.length > 100) {
-        elem.style.fontSize = "15px";
-        elem.style.height = "200px !important";
-        console.log(elem.style.Height);
-
-      } else {
-        elem.style.fontSize = "18px";
-        elem.style.height = "60px !important";
-      }
-      listDOM.appendChild(elem);
-      document.getElementById('addStackText').value = "";
-      // document.getElementById('addStackText').blur();
+      list.appendChild(elem);
+      textarea.value = "";
       return false;
     } else {
       return true;
@@ -91,128 +47,90 @@ import './popup.css';
     addTextArea.style.display = "none";
   });
 
-
-  // 
-  const stackStorage = {
-    get: callback => {
-      chrome.storage.sync.get(['raw'], result => {
-        callback(result.raw);
-      });
-    },
-    set: (value, callback) => {
-      chrome.storage.sync.set({
-          raw: value,
-        },
-        () => {
-          callback();
-        });
-    }
-  };
-
-  function restoreItemList() {
-    stackStorage.get(raw => {
-      if (typeof raw === "undefined") {
-        stackStorage.set(JSON.stringify([]), () => {
-          setupItemList([]);
-        });
-      } else {
-        setupItemList(JSON.parse(raw));
-      }
-      // Create a "close" button and append it to each list item
-      var myNodelist = document.getElementsByTagName("li");
-      var i;
-      for (i = 0; i < myNodelist.length; i++) {
-        var span = document.createElement("SPAN");
-        span.className = "close";
-
-        let iconDOM = document.createElement("i");
-        iconDOM.setAttribute("class", "material-icons");
-        iconDOM.innerText = "delete";
-
-        span.appendChild(iconDOM);
-
-        myNodelist[i].appendChild(span);
-      }
-
-    });
-  }
-
-
-
-  function setupItemList(raw = []) {
-    let listDOM = document.getElementsByClassName('itemlist')[0];
-    raw.forEach(res => {
-      var elem = document.createElement("li");
-      elem.textContent = res.text;
-      elem.addEventListener("dblclick", () => {
-        console.log("!!!")
-        var nodes = document.getElementsByTagName('li');
-        var parent = document.getElementsByTagName('ul')[0];
-        // plus 1
-        parent.removeChild(nodes[nodes.length - 1]);
-      })
-
-      if (elem.textContent.length > 100) {
-        elem.style.fontSize = "15px";
-        elem.style.height = "200px !important";
-        console.log(elem.style.Height);
-
-      } else {
-        elem.style.fontSize = "18px";
-        elem.style.height = "60px !important";
-      }
-
-      listDOM.appendChild(elem);
-      console.log(res.text);
-    });
-
-    document.getElementById('resetBtn').addEventListener('click', () => {
-      updateStack({
-        type: 'RESET'
-      });
-      let listDOM = document.getElementsByClassName('itemlist')[0];
-      while (listDOM.firstChild) {
-        listDOM.removeChild(listDOM.firstChild);
-      }
-      clearForm();
-    });
-  }
-
-  function clearForm() {
-    document.getElementById('addStackText').value = "";
-    document.getElementById('addStackText').value = document.getElementById('addStackText').value.replace(/^(\r\n)|(\n)/, '');
-  }
-
-  function updateStack({
-    type,
-    text = "",
-    url = ""
-  }) {
-    if (type === 'RESET') {
-      stackStorage.set("[]", () => {
-        console.log("Reset!");
-      });
-    } else {
-      stackStorage.get(raw => {
-        // read from storage
-        let itemlist = [];
-        let newItem = {
-          text: text,
-          url: url
-        };
-        if (raw !== undefined) {
-          itemlist = JSON.parse(raw);
-          console.log(itemlist);
-        }
-        itemlist.push(newItem);
-        stackStorage.set(JSON.stringify(itemlist), () => {
-          console.log("Added: " + newItem.text + '&' + newItem.url);
-        });
-      });
-    }
-  };
-
-
   //
   document.addEventListener('DOMContentLoaded', restoreItemList);
+
 })();
+
+
+function restoreItemList() {
+  stackStorage.get(raw => {
+    if (typeof raw === "undefined") {
+      stackStorage.set(JSON.stringify([]), () => {
+        setupItemList([]);
+      });
+    } else {
+      setupItemList(JSON.parse(raw));
+    }
+  });
+}
+
+function setupItemList(raw = []) {
+  raw.forEach(res => {
+    var elem = document.createElement("li");
+    elem.textContent = res.text;
+    list.appendChild(elem);
+    console.log(res.text);
+  });
+
+  resetBtn.addEventListener('click', () => {
+    updateStack({
+      type: 'RESET'
+    });
+    while (list.firstChild) {
+      list.removeChild(list.firstChild);
+    }
+    clearForm();
+  });
+}
+
+
+const stackStorage = {
+  get: callback => {
+    chrome.storage.sync.get(['raw'], result => {
+      callback(result.raw);
+    });
+  },
+  set: (value, callback) => {
+    chrome.storage.sync.set({
+        raw: value,
+      },
+      () => {
+        callback();
+      });
+  }
+};
+
+
+function clearForm() {
+  textarea.value = "";
+}
+
+function updateStack({
+  type,
+  text = "",
+  url = ""
+}) {
+  if (type === 'RESET') {
+    stackStorage.set("[]", () => {
+      console.log("Reset!");
+    });
+  } else {
+    stackStorage.get(raw => {
+      // read from storage
+      let itemlist = [];
+      let newItem = {
+        text: text,
+        url: url
+      };
+      if (raw !== undefined) {
+        itemlist = JSON.parse(raw);
+        console.log(itemlist);
+      }
+      itemlist.push(newItem);
+      stackStorage.set(JSON.stringify(itemlist), () => {
+        console.log("Added: " + newItem.text + '&' + newItem.url);
+      });
+    });
+  }
+};
