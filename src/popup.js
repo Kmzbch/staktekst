73772e921215863e031bd1b0b7f2bb030a0f3d12
Kei Mizubuchi <kmizubuc@my.centennialcollege.ca>
2019-12-15 20:21:52 -0500

    added item filter

--- src/popup.js
+++ src/popup.js
@@ -7,6 +7,9 @@ const footer = document.getElementsByTagName("footer")[0];
 const resetBtn = document.querySelector('#resetBtn');
 const itemListDOM = document.querySelector('.itemlist');
 
+const searchbox = document.querySelector('#searchbox');
+
+
 let itemList = [];
 
 const updateItemListDOM = item => {
@@ -31,27 +34,57 @@ const updateItemListDOM = item => {
     });
   });
 
-  // initialize eventListeners
-  textarea.addEventListener("keyup", (e) => {
-    if (e.keyCode === 13) {
-      updateStack(textarea.value);
-      updateItemListDOM(textarea.value);
-      textarea.value = "";
-      return false;
-    }
-  });
-
-  resetBtn.addEventListener('click', () => {
-    stackStorage.reset();
-    while (itemListDOM.firstChild) {
-      itemListDOM.removeChild(itemListDOM.firstChild);
-    }
-    textarea.value = "";
-    itemList = [];
-  });
-
 })();
 
+// // 削除機能
+// list.addEventListener('click', e => {
+//   if (e.target.classList.contains('delete')) {
+//       e.target.parentElement.remove();
+//       // ########## 追加 ###########
+//       const task = e.target.parentElement.textContent.trim()
+//       deleteTaskFromLocalStorage(task);
+//   }
+// });
+// const deleteTaskFromLocalStorage = task => {
+//   localStorage.removeItem(task);
+//   return;
+// }
+
+const filterTasks = (term) => {
+  Array.from(itemListDOM.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.add('filtered'));
+
+  Array.from(itemListDOM.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.remove('filtered'));
+};
+
+searchbox.addEventListener('keyup', () => {
+  // 空白削除かつ、小文字に変換(大文字・小文字の区別をなくす)
+  const term = searchbox.value.trim().toLowerCase();
+  filterTasks(term);
+})
+
+// initialize eventListeners
+textarea.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    const item = textarea.value.trim();
+    updateStack(item);
+    updateItemListDOM(item);
+    textarea.value = "";
+    return false;
+  }
+});
+
+resetBtn.addEventListener('click', () => {
+  stackStorage.reset();
+  while (itemListDOM.firstChild) {
+    itemListDOM.removeChild(itemListDOM.firstChild);
+  }
+  textarea.value = "";
+  itemList = [];
+});
 
 function updateStack(text) {
   itemList.push({
@@ -66,13 +99,10 @@ const stackStorage = {
       callback(result.raw);
     });
   },
-  set: (value, callback) => {
+  set: (value) => {
     chrome.storage.sync.set({
-        raw: value,
-      },
-      () => {
-        callback();
-      });
+      raw: value,
+    });
   },
   reset: () => {
     chrome.storage.sync.set({
