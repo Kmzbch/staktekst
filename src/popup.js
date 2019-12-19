de0af8c5a07f13c8dcb0f371a8c6d735d55ec497
Kei Mizubuchi <kmizubuc@my.centennialcollege.ca>
2019-12-19 17:59:51 -0500

    refactored updateStackDOM and css

--- src/popup.js
+++ src/popup.js
@@ -1,23 +1,24 @@
 import './popup.css';
 
-const header = document.getElementsByTagName("header")[0];
-const footer = document.getElementsByTagName("footer")[0];
+const header = document.getElementsByTagName('header')[0];
+const footer = document.getElementsByTagName('footer')[0];
 const searchbox = document.querySelector('.searchbox');
 const searchCancelBtn = document.querySelector('.search-cancel');
 const results = document.querySelector('.results');
 
-const addBtn = document.querySelector(".add");
+const addBtn = document.querySelector('.add');
 const sortBy = document.querySelector('.sort-by');
-const addTextItem = document.querySelector(".add-textitem");
+const addTextItem = document.querySelector('.add-textitem');
 
 const textStackDOM = document.querySelector('.textstack');
 const resetBtn = document.querySelector('#resetBtn');
 
 let textStack = [];
 
+/* module-specific utilities */
 function restoreTextStack() {
   stackStorage.get(raw => {
-    if (typeof raw === "undefined") {
+    if (typeof raw === 'undefined') {
       stackStorage.reset();
     } else {
       textStack = JSON.parse(raw);
@@ -28,9 +29,92 @@ function restoreTextStack() {
   });
 };
 
+const updateTextStackDOM = (item, url = "", title = "") => {
+  const MAX_LENGTH = 40;
+  let abbreviation = title.length > MAX_LENGTH ? `${title.substring(0, MAX_LENGTH)}...` : title;
+
+  let html = `
+    <li>
+    ${item}
+    <i class="material-icons deleteItem">check</i>
+    <div class="spacer">
+    <div class="citation">
+    <a class="source" href="${url}" target="_blank">${abbreviation}</a>
+    </div>
+    </li>
+    `;
+
+  textStackDOM.innerHTML += html;
+
+  // increase the hight to avoid overflow
+  let lastChild = textStackDOM.lastElementChild;
+
+  while (isOverflown(lastChild)) {
+    let replacement = document.createElement('li');
+    replacement.innerHTML = lastChild.innerHTML;
+    replacement.style.height = lastChild.offsetHeight + 10 + "px";
+    textStackDOM.removeChild(lastChild);
+    textStackDOM.appendChild(replacement);
+    lastChild = replacement;
+  }
+
+  // consider text item without url as a note
+  if (url === "") {
+    lastChild.className += "note";
+  }
+}
+
+function sortByOld(byOld = false) {
+  if (byOld) {
+    sortBy.innerHTML = 'Old <i class="material-icons">arrow_downward</i>';
+    textStackDOM.style.flexDirection = 'column';
+  } else {
+    sortBy.innerHTML = 'New <i class="material-icons">arrow_upward</i>';
+    textStackDOM.style.flexDirection = 'column-reverse';
+  }
+}
+
+function updateStack(text, url = "", title = "") {
+  textStack.push({
+    text,
+    url,
+    title
+  });
+  stackStorage.set(JSON.stringify(textStack));
+};
+
+const filterTextItems = (term) => {
+  const termRegex = new RegExp(`\\b(${escapeRegExp(term)})(.*?)\\b`, 'ig');
+  let hitCount = 0;
+
+  Array.from(textStackDOM.children).forEach(item => {
+    item.innerHTML = item.innerHTML.replace(/<span class="highlighted">(.*?)<\/span>/g, '$1');
+  });
+
+  // filter unmatched items
+  Array.from(textStackDOM.children)
+    .filter(item => !item.textContent.match(termRegex))
+    .forEach(item => item.classList.add('filtered'));
+
+  // matched items
+  Array.from(textStackDOM.children)
+    .filter(item => item.textContent.match(termRegex))
+    .forEach(item => {
+      item.classList.remove('filtered');
+      // add highlight
+      if (term.length >= 1) {
+        let endIndex = item.innerHTML.search(/<i class="material-icons deleteItem">check<\/i>/i);
+        let targetText = item.innerHTML.substring(0, endIndex);
+        let rest = item.innerHTML.substring(endIndex, item.innerHTML.length)
+        item.innerHTML = targetText.replace(termRegex, "<span class='highlighted'>$1</span>$2") + rest;
+      }
+      hitCount++;
+    });
+  results.textContent = hitCount === 0 ? 'No Results' : `${hitCount} of ${textStackDOM.children.length}`;
+};
+
 /* initialize events */
 function initializeEventListeners() {
-  /* window */
   window.onscroll = () => {
     // hide header and footer except on the top, bottom and on hover
     if (window.pageYOffset == 0) {
@@ -71,7 +155,7 @@ function initializeEventListeners() {
     }
   });
 
-  searchCancelBtn.addEventListener("click", () => {
+  searchCancelBtn.addEventListener('click', () => {
     searchbox.value = "";
     searchbox.dispatchEvent(new Event('input'));
   })
@@ -85,19 +169,18 @@ function initializeEventListeners() {
     addTextItem.focus();
   });
 
-  addTextItem.addEventListener('focusout', (e) => {
+  addTextItem.addEventListener('focusout', () => {
     addTextItem.style.display = 'none';
-
     addBtn.style.display = 'block';
     sortBy.style.display = 'inline-flex';
   });
 
-  addTextItem.addEventListener("keyup", (e) => {
+  addTextItem.addEventListener('keyup', (e) => {
     if (e.keyCode === 13) {
       const item = addTextItem.value.trim();
       updateStack(item);
       updateTextStackDOM(item);
-      addTextItem.value = "";
+      addTextItem.value = '';
 
       return false;
     }
@@ -112,67 +195,28 @@ function initializeEventListeners() {
     while (textStackDOM.firstChild) {
       textStackDOM.removeChild(textStackDOM.firstChild);
     }
-    addTextItem.value = "";
+    addTextItem.value = '';
     textStack = [];
   });
 }
 
-function sortByOld(byOld = false) {
-  if (byOld) {
-    sortBy.innerHTML = 'Old <i class="material-icons">arrow_downward</i>';
-    textStackDOM.style.flexDirection = 'column';
-  } else {
-    sortBy.innerHTML = 'New <i class="material-icons">arrow_upward</i>';
-    textStackDOM.style.flexDirection = 'column-reverse';
+const stackStorage = {
+  get: callback => {
+    chrome.storage.sync.get(['raw'], result => {
+      callback(result.raw);
+    });
+  },
+  set: (value) => {
+    chrome.storage.sync.set({
+      raw: value,
+    });
+  },
+  reset: () => {
+    chrome.storage.sync.set({
+      raw: '[]'
+    });
   }
-}
-
-
-
-const updateTextStackDOM = (item, url = "", title = "") => {
-  let abbreviation = title.length > 40 ? `${title.substring(0, 40)}...` : title;
-
-  let html;
-  if (url === "") {
-    html = `
-    <li class="note">
-    ${item}
-    <i class="material-icons deleteItem">check</i>
-    <div class="spacer">
-    <div class="cardBottom">
-    <span class="source">note</span>
-    </div>
-    </li>
-    `;
-
-  } else {
-    html = `
-    <li>
-    ${item}
-    <i class="material-icons deleteItem">check</i>
-    <div class="spacer">
-    <div class="cardBottom">
-    <a class="source" href="${url}" target="_blank">${abbreviation}</a>
-    </div>
-    </li>
-    `;
-  }
-  textStackDOM.innerHTML += html;
-  // increase the hight to avoid overflow
-  let lastChild = textStackDOM.lastElementChild;
-
-  while (isOverflown(lastChild)) {
-    let replacement = document.createElement('li');
-    replacement.innerHTML = lastChild.innerHTML;
-    replacement.style.height = lastChild.offsetHeight + 10 + "px";
-    textStackDOM.removeChild(lastChild);
-    textStackDOM.appendChild(replacement);
-    lastChild = replacement;
-  }
-}
-
-
-
+};
 
 /* utilities */
 const isOverflown = ({
@@ -193,70 +237,34 @@ function escapeRegExp(string) {
     string;
 }
 
-function updateStack(text, url = "", title = "") {
-  textStack.push({
-    text,
-    url,
-    title
-  });
-  stackStorage.set(JSON.stringify(textStack));
-};
-
-const filterTextItems = (term) => {
-  let hitCount = 0;
-  let termRegex = new RegExp(`\\b(${escapeRegExp(term)})(.*?)\\b`, 'ig');
-
-  // remove highlight
-  Array.from(textStackDOM.children).forEach(item => {
-    item.innerHTML = item.innerHTML.replace(/(<span class="highlight">|<\/span>)/g, '');
-  });
-
-  // filter unmatched items
-  Array.from(textStackDOM.children)
-    .filter(item => !item.textContent.match(termRegex))
-    .forEach(item => item.classList.add('filtered'));
-
-  // matched items
-  Array.from(textStackDOM.children)
-    .filter(item => item.textContent.match(termRegex))
-    .forEach(item => {
-      item.classList.remove('filtered');
-      // add highlight
-      if (term.length >= 1) {
-        let remainingIndex = item.innerHTML.search(/<i class="material-icons deleteItem">check<\/i>/i);
-        let targetText = item.innerHTML.substring(0, remainingIndex);
-        let remaining = item.innerHTML.substring(remainingIndex, item.innerHTML.length)
-        item.innerHTML = targetText.replace(termRegex, "<span class='highlight'>$1</span>$2") + remaining;
-      }
-      hitCount++;
-    });
-
-  results.textContent = hitCount === 0 ? 'No Results' : `${hitCount} of ${textStackDOM.children.length}`;
-
-};
-
-
-
-
-const stackStorage = {
-  get: callback => {
-    chrome.storage.sync.get(['raw'], result => {
-      callback(result.raw);
-    });
-  },
-  set: (value) => {
-    chrome.storage.sync.set({
-      raw: value,
-    });
-  },
-  reset: () => {
-    chrome.storage.sync.set({
-      raw: '[]'
-    });
-  }
-};
-
+/* */
 document.addEventListener('DOMContentLoaded', () => {
   restoreTextStack()
   initializeEventListeners();
+
+
+  setTimeout(() => {
+    let deleteBoxes = document.querySelectorAll('.deleteItem');
+
+    console.log(deleteBoxes);
+    for (let i = 0; i < deleteBoxes.length; i++) {
+      deleteBoxes[i].addEventListener('click', () => {
+        deleteBoxes[i].style = 'color: white !important';
+        console.log(deleteBoxes[i]);
+        // deleteBoxes[i].parentElement.style = ''
+        deleteBoxes[i].parentElement.className += 'deleted';
+        // setTimeout(() => {
+        //   removeTextItem();
+        // }, 500);
+      })
+    }
+
+  }, 30)
+
 });
+
+// function removeTextItem() {
+//   Array.from(textStackDOM.children)
+//   .filter(item => !item.textContent.includes('deleted'))
+//   .forEach(item => item.classList.add('filtered'));
+// }
