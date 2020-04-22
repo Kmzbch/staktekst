88ed307cbbbe6f6672319f27e4939afab43a5e5b
Kei Mizubuchi <kmizubuc@my.centennialcollege.ca>
2020-04-21 22:42:30 -0400

    quick fix for overlay bug

--- src/currentTab.js
+++ src/currentTab.js
@@ -1,20 +1,26 @@
 'use strict';
 
-import * as bubble_lib from './bubble_lib.js';
-
-// overlay
+// attach overlay to the current page
 let overlay = document.createElement('div');
 overlay.id = 'overlay';
 overlay.className = 'hidden';
 document.body.appendChild(overlay);
 
+// turn the overlay off when clicked
+overlay.addEventListener('click', (e) => {
+    if (!overlay.classList.contains('hidden')) {
+        overlay.classList.add('hidden');
+    }
+})
+
+// attach event to onMessage
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log('received!');
     if (request.command === 'OVERLAY_ON') {
         overlay.classList.remove('hidden');
     } else if (request.command === 'OVERLAY_OFF') {
         overlay.classList.add('hidden');
     }
     sendResponse({});
+
     return true;
 });
