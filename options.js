d92af184a6a3b0a80fb89fe70bf6807f1018a7b6
Kei Mizubuchi <kmizubuc@my.centennialcollege.ca>
2020-05-17 14:18:39 -0400

    set locales for options.html

--- options.js
+++ options.js
@@ -1,3 +1,22 @@
+const localizeHtmlPage = () => {
+	//Localize by replacing __MSG_***__ meta tags
+	var objects = document.getElementsByTagName('html');
+	for (var j = 0; j < objects.length; j++) {
+		var obj = objects[j];
+
+		var valStrH = obj.innerHTML.toString();
+		var valNewH = valStrH.replace(/__MSG_(\w+)__/g, function(match, v1) {
+			return v1 ? chrome.i18n.getMessage(v1) : '';
+		});
+
+		if (valNewH != valStrH) {
+			obj.innerHTML = valNewH;
+		}
+	}
+};
+
+localizeHtmlPage();
+
 const preset = {
 	restoreEnabled: true,
 	duration: 30,
